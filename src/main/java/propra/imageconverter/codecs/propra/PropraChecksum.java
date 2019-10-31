00ae7487313ba086be01901e2c51869019f87cf2
Marvin Bruns <android.bruns@googlemail.com>
2019-10-31 21:19:33 +0100

    checksum streamed

--- src/main/java/propra/imageconverter/codecs/propra/PropraChecksum.java
+++ src/main/java/propra/imageconverter/codecs/propra/PropraChecksum.java
@@ -1,52 +1,143 @@
 package propra.imageconverter.codecs.propra;
 
-// XXX Besser auf Streams umstellen, bisher keine Lösung gefunden
-// Weg über Arrays ist leider wegen der Checksum-Berechnung notwendig, sie einen Zugriff per Index auf die Daten verlangt
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
- * Hilfsklasse zum Berechnen und Überprüfen der Checksumme
+ * Hilfsklasse zum Berechnen und Überprüfen der Checksumme (teils inspiriert
+ * durch Review der ersten Abgabe)
  *
  * @author marvin
  *
  */
 public class PropraChecksum {
 
-    private static final int X = 65513;
-
-    private final byte[] data;
-
-    public PropraChecksum(byte[] data) {
-	this.data = (data != null) ? data : new byte[0];
-    }
-
-    public long calculate() {
-	final long n = this.data.length;
+	private static final int X = 65513;
 
 	long a = 0;
 	long b = 1;
 	long prevB = 0;
 
-	for (int i = 0; i < n; i++) {
-	    a = a + ((i + 1) + Byte.toUnsignedInt(this.data[i]));
-	    prevB = b;
-	    b = (prevB + a) % X;
+	long i = 0;
+
+	public void pushByte(int data) {
+		a = a + ((i + 1) + data);
+		prevB = b;
+		b = (prevB + a) % X;
+
+		i++;
 	}
-	a = a % X;
 
-	final long p = (a * ((long) Math.pow(2, 16))) + b;
+	public long getActualChecksum() {
+		long aEnd = a % X;
+		long bEnd = b;
+		final long p = (aEnd * ((long) Math.pow(2, 16))) + bEnd;
+		return p;
+	}
 
-	return p;
-    }
+	public static class PropraChecksumInputStream extends InputStream {
 
-    /**
-     * Berechnet die Checksumme (siehe Aufgabenstellung) für ein Byte-Array
-     *
-     * @param data Die zu grunde liegenden Daten
-     * @return Die Checksumme
-     */
-    public static long calculateChecksum(byte[] data) {
-	final PropraChecksum checksum = new PropraChecksum(data);
+		private InputStream in;
+		private PropraChecksum checksum = new PropraChecksum();
 
-	return checksum.calculate();
-    }
+		public PropraChecksumInputStream(InputStream in) {
+			super();
+			this.in = in;
+		}
+
+		@Override
+		public int read() throws IOException {
+			int data = in.read();
+			if (data < 0) {
+				return data;
+			}
+
+			checksum.pushByte(data);
+			return data;
+		}
+
+		public long getActualChecksum() {
+			return checksum.getActualChecksum();
+		}
+
+		/**
+		 * Schließt ebenfalls den übergebenen InputStream
+		 */
+		@Override
+		public void close() throws IOException {
+			super.close();
+			in.close();
+		}
+
+		/**
+		 * Liest alle bytes ohne etwas damit zu tun.
+		 * 
+		 * @throws IOException
+		 */
+		public void consumeAllBytes() throws IOException {
+			while (read() >= 0) {
+				continue;
+			}
+		}
+
+	}
+
+	public static class PropraChecksumOutputStream extends OutputStream {
+
+		private OutputStream out;
+		private PropraChecksum checksum = new PropraChecksum();
+
+		public PropraChecksumOutputStream(OutputStream out) {
+			super();
+			this.out = out;
+		}
+
+		@Override
+		public void write(int data) throws IOException {
+			data = data & 0xFF;
+			
+			out.write(data);
+			checksum.pushByte(data);
+		}
+
+		public long getActualChecksum() {
+			return checksum.getActualChecksum();
+		}
+
+		/**
+		 * Schließt ebenfalls den übergebenen OutputStream
+		 */
+		@Override
+		public void close() throws IOException {
+			super.close();
+			out.close();
+		}
+
+	}
+
+	/**
+	 * Berechnet die Checksumme (siehe Aufgabenstellung) für ein Byte-Array
+	 * 
+	 * 
+	 * @deprecated besser direkt die Streamvariante nutzen z.B.
+	 *             {@link PropraChecksumInputStream} oder
+	 *             {@link PropraChecksumOutputStream}
+	 * @param data Die zu grunde liegenden Daten
+	 * @return Die Checksumme
+	 */
+	public static long calculateChecksum(byte[] data) {
+		PropraChecksumInputStream in = new PropraChecksumInputStream(new ByteArrayInputStream(data));
+
+		try {
+			in.consumeAllBytes();
+			in.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		return in.getActualChecksum();
+	}
 
 }
--- src/test/java/propra/imageconverter/codecs/propra/PropraChecksumTest.java
+++ src/test/java/propra/imageconverter/codecs/propra/PropraChecksumTest.java
@@ -2,12 +2,19 @@ package propra.imageconverter.codecs.propra;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import org.junit.Test;
 
+import propra.imageconverter.codecs.propra.PropraChecksum.PropraChecksumInputStream;
+import propra.imageconverter.codecs.propra.PropraChecksum.PropraChecksumOutputStream;
+
 public class PropraChecksumTest {
 
+	@SuppressWarnings("deprecation")
 	@Test
 	public void testTextChecksum() throws Exception {
 		assertEquals(0x00750076, PropraChecksum.calculateChecksum("t".getBytes(StandardCharsets.UTF_8)));
@@ -24,6 +31,7 @@ public class PropraChecksumTest {
 
 	}
 
+	@SuppressWarnings("deprecation")
 	@Test
 	public void testByteChecksum() throws Exception {
 		assertEquals(0x00000001, PropraChecksum.calculateChecksum(new byte[] {}));
@@ -32,6 +40,77 @@ public class PropraChecksumTest {
 		assertEquals(0x00040006, PropraChecksum.calculateChecksum(new byte[] { 0, 1 }));
 		assertEquals(0x00040007, PropraChecksum.calculateChecksum(new byte[] { 1, 0 }));
 		assertEquals(0x01820283, PropraChecksum.calculateChecksum(new byte[] { (byte) 255, (byte) 128 }));
+	}
+	
+	@Test
+	public void testTextChecksumInputStream() throws Exception {
+		assertEquals(0x00750076, checksumByInputStream("t".getBytes(StandardCharsets.UTF_8)));
+		assertEquals(0x00DC0152, checksumByInputStream("te".getBytes(StandardCharsets.UTF_8)));
+		assertEquals(0x015202A4, checksumByInputStream("tes".getBytes(StandardCharsets.UTF_8)));
+		assertEquals(0x01CA046E, checksumByInputStream("test".getBytes(StandardCharsets.UTF_8)));
+
+		assertEquals(0x3C56F024, checksumByInputStream(
+				"Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+						.getBytes(StandardCharsets.UTF_8)));
+		assertEquals(0x07AEE0D6, checksumByInputStream(
+				"Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+						.getBytes(StandardCharsets.UTF_8)));
+
+	}
+
+	@Test
+	public void testByteChecksumInputStream() throws Exception {
+		assertEquals(0x00000001, checksumByInputStream(new byte[] {}));
+		assertEquals(0x00010002, checksumByInputStream(new byte[] { 0 }));
+		assertEquals(0x00020003, checksumByInputStream(new byte[] { 1 }));
+		assertEquals(0x00040006, checksumByInputStream(new byte[] { 0, 1 }));
+		assertEquals(0x00040007, checksumByInputStream(new byte[] { 1, 0 }));
+		assertEquals(0x01820283, checksumByInputStream(new byte[] { (byte) 255, (byte) 128 }));
+	}
+	
+	long checksumByInputStream(byte[] data) throws IOException {
+		PropraChecksumInputStream inputStream = new PropraChecksum.PropraChecksumInputStream(new ByteArrayInputStream(data));
+		inputStream.consumeAllBytes();
+		long actualChecksum = inputStream.getActualChecksum();
+		inputStream.close();
+		
+		return actualChecksum;
+	}
+	
+	@Test
+	public void testTextChecksumOutputStream() throws Exception {
+		assertEquals(0x00750076, checksumByOutputStream("t".getBytes(StandardCharsets.UTF_8)));
+		assertEquals(0x00DC0152, checksumByOutputStream("te".getBytes(StandardCharsets.UTF_8)));
+		assertEquals(0x015202A4, checksumByOutputStream("tes".getBytes(StandardCharsets.UTF_8)));
+		assertEquals(0x01CA046E, checksumByOutputStream("test".getBytes(StandardCharsets.UTF_8)));
+
+		assertEquals(0x3C56F024, checksumByOutputStream(
+				"Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+						.getBytes(StandardCharsets.UTF_8)));
+		assertEquals(0x07AEE0D6, checksumByOutputStream(
+				"Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+						.getBytes(StandardCharsets.UTF_8)));
+
+	}
+
+	@Test
+	public void testByteChecksumOutputStream() throws Exception {
+		assertEquals(0x00000001, checksumByOutputStream(new byte[] {}));
+		assertEquals(0x00010002, checksumByOutputStream(new byte[] { 0 }));
+		assertEquals(0x00020003, checksumByOutputStream(new byte[] { 1 }));
+		assertEquals(0x00040006, checksumByOutputStream(new byte[] { 0, 1 }));
+		assertEquals(0x00040007, checksumByOutputStream(new byte[] { 1, 0 }));
+		assertEquals(0x01820283, checksumByOutputStream(new byte[] { (byte) 255, (byte) 128 }));
+	}
+	
+	long checksumByOutputStream(byte[] data) throws IOException {
+		PropraChecksumOutputStream ouputStream = new PropraChecksum.PropraChecksumOutputStream(new ByteArrayOutputStream(data.length));
+		ouputStream.write(data);
+		
+		long actualChecksum = ouputStream.getActualChecksum();
+		ouputStream.close();
+		
+		return actualChecksum;
 	}
 
 }
