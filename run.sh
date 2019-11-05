6ecc2f1889114e079d20144227d4c6818c746af0
brunothg <android.bruns@googlemail.com>
2019-11-05 18:13:42 +0100

    shell script

--- run.sh
+++ run.sh
@@ -54,9 +54,11 @@ java -Xmx256m propra.imageconverter.ImageConverter --output=../KE1_TestBilder/te
 java -Xmx256m propra.imageconverter.ImageConverter --output=../KE1_TestBilder/test_02_uncompressed2.tga --input=../KE1_Konvertiert/test_02.png
 java -Xmx256m propra.imageconverter.ImageConverter --input=../KE1_TestBilder/test_02_uncompressed2.tga --output=../KE1_Konvertiert/test_02_02.png
 
+wait
+cd ../..
+ls -l
 
-
-# Build
+# Build Teil 2
 rm KE2_Bruns_Marvin.zip
 
 mkdir tmp
@@ -80,17 +82,58 @@ cd KE2_Bruns_Marvin
 mkdir KE2_Konvertiert
 unzip ../KE2_Bruns_Marvin.zip
 unzip ../KE2_TestBilder.zip
+unzip ../KE2_TestBilder_optional.zip
 
 javac --source-path src src/propra/imageconverter/ImageConverter.java -d bin
 cd bin
 
 java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_01_uncompressed.tga --output=../KE2_Konvertiert/test_01.propra --compression=rle
+java -Xmx256m propra.imageconverter.ImageConverter --output=../KE2_Konvertiert/test_01_uncompressed.png --input=../KE2_Konvertiert/test_01.propra
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_01_uncompressed.tga --output=../KE2_Konvertiert/test_01.tga --compression=rle
+java -Xmx256m propra.imageconverter.ImageConverter --output=../KE2_Konvertiert/test_01_uncompressed_2.tga --input=../KE2_Konvertiert/test_01.propra --compression=uncompressed
+
 java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_02_rle.tga          --output=../KE2_Konvertiert/test_02.propra --compression=uncompressed
+java -Xmx256m propra.imageconverter.ImageConverter --output=../KE2_Konvertiert/test_02.propra          --input=../KE2_Konvertiert/test_02.propra --compression=uncompressed
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_02_rle.tga          --output=../KE2_Konvertiert/test_02.png
+
 java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_03_uncompressed.propra --output=../KE2_Konvertiert/test_03.tga --compression=rle
+java -Xmx256m propra.imageconverter.ImageConverter --output=../KE2_Konvertiert/test_03.png --input=../KE2_Konvertiert/test_03.tga
+java -Xmx256m propra.imageconverter.ImageConverter --output=../KE2_Konvertiert/test_03_rle.propra --input=../KE2_Konvertiert/test_03.tga --compression=rle
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_Konvertiert/test_03_rle.propra --output=../KE2_Konvertiert/test_03_2.tga --compression=rle
+
 java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_04_rle.propra          --output=../KE2_Konvertiert/test_04.tga --compression=uncompressed
-java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_05_base32.tga.base-32    --decode-base-32
-java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_06_base32.propra.base-32 --decode-base-32
-java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_02_rle.tga               --encode-base-32
-java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_04_rle.propra            --encode-base-32
+java -Xmx256m propra.imageconverter.ImageConverter --output=../KE2_Konvertiert/test_04.png          --input=../KE2_Konvertiert/test_04.tga
+java -Xmx256m propra.imageconverter.ImageConverter --output=../KE2_Konvertiert/test_04_2.tga          --input=../KE2_Konvertiert/test_04.tga --compression=uncompressed
 
 
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_05_base32.tga.base-32    --decode-base-32 &
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_06_base32.propra.base-32 --decode-base-32 &
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_02_rle.tga               --encode-base-32 &
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder/test_04_rle.propra            --encode-base-32 &
+
+wait
+
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder_optional/test_base-2_a.propra.base-n --decode-base-n &
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder_optional/test_base-2_b.propra.base-n --decode-base-n &
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder_optional/test_base-4.propra.base-n --decode-base-n &
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder_optional/test_base-8.propra.base-n --decode-base-n &
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder_optional/test_base-64.propra.base-n --decode-base-n &
+
+wait
+
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder_optional/test_base-2_a.propra --output=../KE2_Konvertiert/test_base-2_a.tga &
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder_optional/test_base-2_b.propra --output=../KE2_Konvertiert/test_base-2_b.tga &
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder_optional/test_base-4.propra --output=../KE2_Konvertiert/test_base-4.tga &
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder_optional/test_base-8.propra --output=../KE2_Konvertiert/test_base-8.tga &
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder_optional/test_base-64.propra --output=../KE2_Konvertiert/test_base-64.tga &
+
+wait
+
+java -Xmx256m propra.imageconverter.ImageConverter --input=../KE2_TestBilder_optional/test_grosses_bild.propra --output=../KE2_Konvertiert/test_grosses_bild.tga --compression=rle &
+
+
+
+
+wait
+cd ../..
+ls -l
